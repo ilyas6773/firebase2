8cb60ba9f635c02e4ce38321a6c7db08b7cc3b67
Ilyas Tolegenov <mister.tolegenov@yandex.ru>
2021-02-18 08:32:31 +0600

    Initial commit

---
+++ app/src/main/java/com/example/firebase2/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.firebase2
+
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import com.google.firebase.database.FirebaseDatabase
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        
+        var firebaseDatabase = FirebaseDatabase.getInstance()
+        var databaseRef = firebaseDatabase.reference
+
+        databaseRef.setValue("Hello There!")
+
+    }
+}
